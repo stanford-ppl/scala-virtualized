ed902669f9d24c27bf213a7826a0a2c15265ad9f
Ruben Fiszel <ruben.fiszel@epfl.ch>
2017-03-27 19:11:06 +0200

    scalameta build file

--- project/Build.scala
+++ project/Build.scala
@@ -1,19 +1,25 @@
-import sbt._
-import Keys._
+import sbt.Keys.resolvers
+import sbt.{Compile, _}
 
 object VirtualizedBuild extends Build {
 
   val compilerVersion = "2.12.1"
   val scalatestVersion = "3.0.1"
-  val paradiseVersion = "2.1.0"  // check here: https://github.com/scalamacros/paradise/releases
+  val paradiseVersion = "3.0.0-M7"  // check here: https://github.com/scalamacros/paradise/releases
+  val metaVersion = "1.6.0"
 
   lazy val virtBuildSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.virtualized",
     scalaVersion := compilerVersion,
 
+    resolvers += Resolver.sonatypeRepo("releases"),
+    resolvers += Resolver.bintrayIvyRepo("scalameta", "maven"),
+
     publishArtifact in (Compile, packageDoc) := false,
     libraryDependencies += "org.scalatest" %% "scalatest" % scalatestVersion % "test",
     libraryDependencies += "org.scala-lang" % "scala-reflect" % scalaVersion.value % "compile",
+    libraryDependencies += "org.scalameta" %% "scalameta" % metaVersion,
+    libraryDependencies += "org.scalameta" %% "contrib" % metaVersion,
 
     retrieveManaged := true,
     scalacOptions += "-Yno-generic-signatures",
@@ -26,7 +32,7 @@ object VirtualizedBuild extends Build {
     scalacOptions ++= Seq("-language:higherKinds", "-language:implicitConversions"),
 
     scalacOptions in (Compile, doc) ++= Seq(
-      "-doc-root-content", 
+      "-doc-root-content",
       baseDirectory.value+"/root-doc.txt",
       "-diagrams",
       "-diagrams-debug",
@@ -34,12 +40,16 @@ object VirtualizedBuild extends Build {
       "-doc-title", name.value
     ),
 
-    addCompilerPlugin("org.scalamacros" % "paradise" % paradiseVersion cross CrossVersion.full),
-  
-//    addCompilerPlugin("org.scalameta" % "paradise" % paradiseVersion cross CrossVersion.full),
+    addCompilerPlugin("org.scalameta" % "paradise" % paradiseVersion cross CrossVersion.full),
 
-    scalaSource in Compile <<= baseDirectory(_ / "src"),
-    scalaSource in Test <<= baseDirectory(_ / "test"),
+    scalacOptions += "-Xplugin-require:macroparadise",
+    // temporary workaround for https://github.com/scalameta/paradise/issues/10
+    scalacOptions in (Compile, console) := Seq(), // macroparadise plugin doesn't work in repl yet.
+    // temporary workaround for https://github.com/scalameta/paradise/issues/55
+    sources in (Compile, doc) := Nil, // macroparadise doesn't work with scaladoc yet.
+
+    scalaSource in Compile := baseDirectory(_/ "src").value,
+    scalaSource in Test := baseDirectory(_/"test").value,
 
     parallelExecution in Test := false,
     concurrentRestrictions in Global += Tags.limitAll(1) // we need tests to run in isolation across all projects
@@ -49,6 +59,4 @@ object VirtualizedBuild extends Build {
     version := "0.2",
     isSnapshot := true 
   )
-
-  lazy val virtualized = Project("virtualized", file("."), settings = virtualizedSettings)
 }
